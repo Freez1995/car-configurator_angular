9a80d67fcc4e7134a3b8abecfdf2c080e95b2c40
Dario Šoć <soc.darioo@gmail.com>
2022-11-08 15:06:45 +0100

    Installed Angular material component plugin, Firebase configuration. Created auth and home modules.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'carconfigapp-804c6',
+    appId: '1:665515613528:web:8ba547ec7aca6b25d7992d',
+    databaseURL: 'https://carconfigapp-804c6-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'carconfigapp-804c6.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBOLux7AlwTyDrcgslmKNoFGA0jCn4kp4o',
+    authDomain: 'carconfigapp-804c6.firebaseapp.com',
+    messagingSenderId: '665515613528',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'carconfigapp-804c6',
+    appId: '1:665515613528:web:8ba547ec7aca6b25d7992d',
+    databaseURL: 'https://carconfigapp-804c6-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'carconfigapp-804c6.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBOLux7AlwTyDrcgslmKNoFGA0jCn4kp4o',
+    authDomain: 'carconfigapp-804c6.firebaseapp.com',
+    messagingSenderId: '665515613528',
+  },
   production: false
 };
 
