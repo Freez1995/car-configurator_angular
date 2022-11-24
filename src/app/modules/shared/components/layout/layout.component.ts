4a03d921ff69716c5ac9833322c5972d369b2d1f
Dario Šoć <soc.darioo@gmail.com>
2022-11-24 16:44:52 +0100

    Shared and Home modules refactor to show/hide button in navigation bar, changed app background color, created sharedService to handle sharing state between components that are not direct child/parent relation.

--- src/app/modules/shared/components/layout/layout.component.ts
+++ src/app/modules/shared/components/layout/layout.component.ts
@@ -1,21 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/modules/auth/services/auth-service.service';
+import { NavigationBarOptions } from '../../models/ISharedService';
 import { ErrorTransformPipe } from '../../pipes/error-transform.pipe';
+import { SharedService } from '../../services/shared.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
-export class LayoutComponent implements OnInit {
+export class LayoutComponent {
+  configureCarButtonShown = true;
+
   constructor(
     private auth: AuthService,
     private router: Router,
     private errorTransform: ErrorTransformPipe,
+    private sharedService: SharedService<NavigationBarOptions>,
     private snackBar: MatSnackBar
-  ) {}
+  ) {
+    this.sharedService.subscribe(
+      (item) => (this.configureCarButtonShown = item.isButtonShown)
+    );
+  }
 
   onLogout() {
     this.auth
@@ -37,6 +46,4 @@ export class LayoutComponent implements OnInit {
   handleNavigateCarSelector() {
     this.router.navigate(['select-car']);
   }
-
-  ngOnInit(): void {}
 }
